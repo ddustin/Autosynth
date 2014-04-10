2a6a7a8887d8f5df68f92dd966b27e0c919c6636
Dustin Dettmer <dustin@apportable.com>
2014-04-09 17:11:30 -0700

    first commit

---
+++ AutoSynth/Loadable/Loadable.h
@@ -0,0 +1,13 @@
+//
+//  Loadable.h
+//  AutoSynth
+//
+//  Created by Dustin Dettmer on 4/9/14.
+//  Copyright (c) 2014 DustyTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Loadable : NSObject
+
+@end
---
+++ AutoSynth/Loadable/Loadable.m
@@ -0,0 +1,13 @@
+//
+//  Loadable.m
+//  AutoSynth
+//
+//  Created by Dustin Dettmer on 4/9/14.
+//  Copyright (c) 2014 DustyTech. All rights reserved.
+//
+
+#import "Loadable.h"
+
+@implementation Loadable
+
+@end
